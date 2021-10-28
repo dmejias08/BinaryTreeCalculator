38f6d09de365db2157c4f5e6adb320b05ec2fc99
dmejias08 <85594834+dmejias08@users.noreply.github.com>
2021-10-27 22:25:48 -0600

    Fixed mistake infixtopostix method

--- src/com/proyecto2/ClientHandler.java
+++ src/com/proyecto2/ClientHandler.java
@@ -49,24 +49,30 @@ public class ClientHandler implements Runnable {
         Stack<Character> operators = new Stack<>();
 
         for (int i = 0; i < exp.length(); i++){
+            System.out.println(operators);
             char ch = exp.charAt(i);
 
             if (Character.isDigit(ch)){
                 result += ch;
+                System.out.println("Soy digit " + ch);
 
             }else if (ch == '('){
                 operators.push(ch);
+                System.out.println("Soy (");
 
                 // pop until find "("
             }else if (ch == ')'){
                 while (!operators.isEmpty() &&
                         operators.peek() != '('){
                     result += operators.pop();
+                    System.out.println("Soy " + result);
+//                    operators.pop();
 
                 }
             }else { // found an operator
                 while (!operators.isEmpty() &&
                         Prec(ch) <= Prec(operators.peek())) {
+                    System.out.println("soy operator " + ch);
                     result += operators.pop();
                 }
                 operators.push(ch);
@@ -74,11 +80,15 @@ public class ClientHandler implements Runnable {
         }
         //pop all
         while (!operators.isEmpty()){
-            if(operators.peek() == '(') {
+            System.out.println("last while");
+            if(operators.peek() == '(' && operators.isEmpty() ) {
                 break;
             }
             else if(operators.peek()!= '(') {
                 result += operators.pop();
+            }
+            else if(operators.peek() == '('){
+                Character trash = operators.pop();
             }
         }
         return result;
