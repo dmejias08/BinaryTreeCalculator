512417899ddc73238e188079d0837f8f0b919c51
geogardow <85602062+geogardow@users.noreply.github.com>
2021-11-03 15:50:02 -0600

    Update ClientHandler
    Se corrigieron errores de infix to postfix , binary tree y evaluate binary tree

--- src/com/proyecto2/ClientHandler.java
+++ src/com/proyecto2/ClientHandler.java
@@ -32,7 +32,7 @@ public class ClientHandler implements Runnable {
                     System.out.println("Closing connection");
                     break;
                 }
-                String response = infixToPosfix(command);
+                String response = infixToPostfix(command);
                 out.writeUTF(response);
             }
         } catch (IOException e) {
@@ -42,64 +42,168 @@ public class ClientHandler implements Runnable {
 
 
 
-    public static String infixToPosfix (String exp){
+    static String infixToPostfix(String exp) {
+        // initializing empty String for result
+        String result = new String("");
 
-        String result = "";
-        // empty stack for operators
+        // initializing empty stack
         Stack<Character> operators = new Stack<>();
 
-        for (int i = 0; i < exp.length(); i++){
-            System.out.println(operators);
-            char ch = exp.charAt(i);
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
 
-            if (Character.isDigit(ch)){
-                result += ch;
-                System.out.println("Soy digit " + ch);
-
-            }else if (ch == '('){
-                operators.push(ch);
-                System.out.println("Soy (");
-
-                // pop until find "("
-            }else if (ch == ')'){
-                while (!operators.isEmpty() &&
-                        operators.peek() != '('){
-                    result += operators.pop();
-                    System.out.println("Soy " + result);
-//                    operators.pop();
-
+            // If the scanned character is an
+            // operand, add it to output.
+            int newPosition = i;
+            if (Character.isLetterOrDigit(c)) {
+                for (int n = i; n < exp.length(); n++) {
+                    char c2 = exp.charAt(n);
+                    if (Character.isLetterOrDigit(c2)) {
+                        result += c2;
+                    } else {
+                        newPosition = n;
+                        break;
+                    }
                 }
-            }else { // found an operator
-                while (!operators.isEmpty() &&
-                        Prec(ch) <= Prec(operators.peek())) {
-                    System.out.println("soy operator " + ch);
-                    result += operators.pop();
+                i = newPosition - 1;
+                result += " ";
+                // If the scanned character is an '(',
+                // push it to the stack.
+            } else if (c == '(') {
+                operators.push(c);
+
+                //  If the scanned character is an ')',
+                // pop and output from the stack
+                // until an '(' is encountered.
+            } else if (c == ')') {
+                while (!operators.isEmpty() && operators.peek() != '(') {
+                    result += operators.pop() + " ";
                 }
-                operators.push(ch);
+                operators.pop();
+            } else {
+                while (!operators.isEmpty() && Prec(c) <= Prec(operators.peek())) {
+                    result += operators.pop();
+                    result += " ";
+                }
+                operators.push(c);
             }
         }
-        //pop all
-        while (!operators.isEmpty()){
-            System.out.println("last while");
-            if(operators.peek() == '(' && operators.isEmpty() ) {
-                break;
+
+        // pop all the operators from the stack
+        while (!operators.isEmpty()) {
+            if (operators.peek() == '(') {
+                return "Invalid Expression";
             }
-            else if(operators.peek()!= '(') {
-                result += operators.pop();
-            }
-            else if(operators.peek() == '('){
-                Character trash = operators.pop();
-            }
+            result += operators.pop() + " ";
         }
+        System.out.println(result);
         return result;
     }
-    static int Prec (char  operator){
-        if (operator == '+' || operator == '-'){
+
+    static int Prec(char operator) {
+        if (operator == '+' || operator == '-') {
             return 1;
-        }
-        else if (operator == '*' || operator == '/'){
+        } else if (operator == '*' || operator == '/') {
             return 2;
         }
         return -1;
+    }
+
+
+    //Node class BinaryTree
+    public class Node {
+        String data;
+        Node left;
+        Node right;
+
+        //Constructor
+        Node(String value) {
+            this.data = value;
+            this.left = this.right = null;
+        }
+    }
+
+    //Creating BinaryTree
+    public class BinaryTree {
+        boolean foundOperator(char op) {
+            if (op == '+' || op == '-' || op == '*' || op == '/') {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        public Node BinaryTree(String exp){
+
+            Stack <Node> nodeStack = new Stack<>();
+            Node node, node1, node2;
+
+
+            for (int j = 0; j < exp.length()-1; j++) {
+                char ch = exp.charAt(j);
+
+                // Found number add to the stack
+                if (foundOperator(ch) == false) {
+                    String actualChildren="";
+                    int newPosition=0;
+                    for (int n = j; n < exp.length()-1; n++) {
+                        char c2 = exp.charAt(n);
+                        if (Character.isLetterOrDigit(c2)) {
+                            actualChildren += c2;
+                        } else {
+                            newPosition=n;
+                            break;
+                        }
+                    }
+
+                    j=newPosition;
+
+                    node = new Node(actualChildren);
+                    nodeStack.push(node);
+
+                } else { //Found operator
+                    node = new Node(""+ch);
+                    j++;
+                    //Add its children
+                    node1 = nodeStack.pop();
+                    node2 = nodeStack.pop();
+
+                    node.left = node1;
+                    node.right = node2;
+
+                    //add father to the stack
+                    nodeStack.push(node);
+                }
+            }
+            node = nodeStack.peek();
+            return node;
+        }
+    }
+    // evaluate binary tress
+    public static int evalBinaryTree(Node root){
+        System.out.println("Root");
+        System.out.println(root.data);
+
+        if(root == null){
+            return 0;
+        }
+
+        if (root.left == null && root.right == null){
+            return Integer.valueOf(root.data);
+        }
+
+        int result=0;
+
+        if (root.data.equals("+")){
+            result = evalBinaryTree(root.right) + evalBinaryTree(root.left);
+        } else if (root.data.equals("-")){
+            result = evalBinaryTree(root.right) - evalBinaryTree(root.left); ;
+        } else if (root.data.equals("*")){
+            result = evalBinaryTree(root.right) * evalBinaryTree(root.left);;
+        } else if (root.data.equals("/")) {
+            result = evalBinaryTree(root.right) / evalBinaryTree(root.left);;
+        } else {
+            result = Integer.valueOf(root.data);
+        }
+        return result;
     }
 }
